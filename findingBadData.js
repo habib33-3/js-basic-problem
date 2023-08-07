65f82d1139a5316beccd5655cd84c674f59d16b7
Habibur Rahman <habib.swi16@gmail.com>
2023-08-07 22:22:51 +0600

    findbaddata

---
+++ findingBadData.js
@@ -0,0 +1,21 @@
+function findBadData(arr) {
+  if (!Array.isArray(arr)) {
+    return "Please enter a array";
+  } else {
+    let count = 0;
+    for (const element of arr) {
+      if (typeof element !== "number") {
+        return "please enter valid number";
+      } else {
+        if (element < 0) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+}
+
+let arr = [3, 4, 5, -2, 6, -6, 5];
+
+console.log(findBadData(arr));
